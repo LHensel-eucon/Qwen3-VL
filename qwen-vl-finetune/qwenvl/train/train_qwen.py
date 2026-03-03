03374833f691d9b2248a007f0e2e79af3bacf909
Laura <laura.hensel@eucondigitalservices.onmicrosoft.com>
2026-03-03 12:47:56 +0100

    Reverted to flash attention

--- qwen-vl-finetune/qwenvl/train/train_qwen.py
+++ qwen-vl-finetune/qwenvl/train/train_qwen.py
@@ -229,6 +229,5 @@ def train(attn_implementation="flash_attention_2"):
 
 
 if __name__ == "__main__":
-    #train(attn_implementation="flash_attention_2")
-    train(attn_implementation="sdpa")
+    train(attn_implementation="flash_attention_2")
 
